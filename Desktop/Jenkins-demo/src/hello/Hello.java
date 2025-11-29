d71ed600360a625fd5dbccae04074ea2f3990afe
M. Sai Deepthi <molugarisaideepthi@gmail.com>
2025-11-29 19:20:24 +0530

    My first commit

---
+++ Desktop/Jenkins-demo/src/hello/Hello.java
@@ -0,0 +1,13 @@
+package hello;
+public class Hello{
+public static void main(String[] args){
+System.out.println("Hello pipeline");
+System.out.println("I'm Deepthi");
+System.out.println("Triggering through Webhooks");
+System.out.println("Hey");
+System.out.println("Plugins");
+System.out.println("Plu");
+
+  
+}
+}
